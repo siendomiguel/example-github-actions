3a9831873d8911083c1a4525554f95523ded4e7d
Miguel Lares <siendomigueldev@gmail.com>
2024-08-21 21:58:21 -0500

    new feature

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ import express from 'express';
 const app = express();
 const PORT = 3000 || process.env.PORT;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
